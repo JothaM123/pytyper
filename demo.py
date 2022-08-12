ef29911528c2f3e04cf41d6ed8c68d1b3e4ec32e
Jotham <jothamsamuel05@gmail.com>
2022-08-12 17:50:03 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,5 @@
+from pytyper import *
+# or import pytyper
+
+pytype("This is my long text with great adjectives and all parts of speech.",0.02)
+# or pytyper.pytype(text,speed)
---
+++ pytyper.py
@@ -0,0 +1,9 @@
+import time
+#import modules 👆👆👆
+
+def pytype(text,speed):
+    for character in text:
+        print(character,end="")
+        time.sleep(speed)
+
+#pytype function 👆👆👆
